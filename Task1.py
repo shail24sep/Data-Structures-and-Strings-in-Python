ad7e269f0202aee1ee43b3ecba9e8e896edbd611
Shailesh Vishwakarma <shail24sep@gmail.com>
2025-05-01 12:08:20 +0530

    Update Task1.py

--- Task1.py
+++ Task1.py
@@ -1,7 +1,6 @@
 student={'Abhishek':80,'Shashank':70,'Rahul':85,'Alice':85}
 myinput=input("Enter the student name's: ")
-checkuser=(myinput in student )
-if(checkuser== True):
-    print(myinput+"'s marks:",student[myinput])
+if myinput in student:
+    print(f"{myinput}'s marks:",student[myinput])
 else:
     print("student not found.")
